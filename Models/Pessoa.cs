1d4a80a58735b3ed04bd19df05c3fb08b61eef76
alcir09 <alcir.amorim9@gmail.com>
2022-01-02 12:01:21 -0300

    POO

---
+++ Models/Pessoa.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Program.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+
+        public void Apresentar(){
+
+            Console.WriteLine($"Olá, meu nome é {Nome} e tenho {Idade} anos");
+        }
+
+    }
+}
